077f7e1799b30e898da739de9278e32eeea69489
Akanksha Pal <akankshapal1055@gmail.com>
2024-09-15 14:18:49 +0530

    login page draft2

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
-// import var1 from "../Assets/dummy.png"
-import loginLogo from "../Assets/logo.png"
+import React from "react";
+import loginLogo from "../Assets/logo.png";
 
-import "./Login.css"
+import "./Login.css";
 import { FaEye } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div className='Login_Body'>
-        {/* <img className = "Logo" src={var1} alt="" /> */}
-        <div className="login_div">
+    <div className="Login_Body">
+      <div className="login_div">
         <div className="login_box">
           <img src={loginLogo} alt="" />
-          <h2>Face liveness  </h2>
+          <h2>Face liveness </h2>
           <h3>Login </h3>
-          <form action="">
-            <div>
-              
-                <input className='Login_input'
-                  type="text"
-                  placeholder="Enter Username or Email"
-                  required
-                />
-            </div>
-  
-            <div className='eyeImg'>
-            
-                <input className='Login_input'
-                  type="password"
-                  placeholder="Enter Your Password"
-                  required
-                />
-                <span className='Login_passIcon'><FaEye /></span>
+          <form action="" className="login_form">
+            <input
+              className="Login_input"
+              type="text"
+              placeholder="Enter Username or Email"
+              required
+            />
 
+            <div className="eyeImg">
+              <input
+                className="Login_input"
+                type="password"
+                placeholder="Enter Your Password"
+                required
+              />
+              <span className="Login_passIcon">
+                <FaEye />
+              </span>
             </div>
-            
+            <button type="submit">Login</button>
           </form>
-          <button type="submit">Login</button>
+        
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
