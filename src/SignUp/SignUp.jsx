d3be9f52aa47f03d36c425706d18bbf503bf5ca5
I-S-H-I-K-A <ishikaverma174@gmail.com>
2024-09-15 15:45:31 +0530

    updated signup page UI

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -5,30 +5,34 @@ import { FaEye } from "react-icons/fa";
 //import password from "../Assets/password.png"
 //import call from "../Assets/call.png"
 //import user from "../Assets/user.png"
+import logo from "../Assets/SignUpLogo.jpg"
 
 export default function SignUp(){
     return(
         <div className="SignUp_body">
             <div className="SignUp_main">
                 <div className="SignUp_header">
-                    <div className="SignUp_text">SIGN UP</div>
-                    <div className="SignUp_underline"></div>
+                  <img src={logo} alt="" />
+
+                    <div className="SignUp_text_main">Face Liveness </div>
+                    <div className="SignUp_text">Sign Up</div>
+                    
                 </div>
                 <div className="SignUp_inputs">
                     <div className="SignUp_input">
-                      <input placeholder="name" type="text" /> 
+                      <input placeholder="Enter Your Name" type="text" /> 
                     </div>
                     <div className="SignUp_input">
-                      <input type="number" placeholder="number" />
+                      <input type="number" placeholder="Enter Your Phone Number" />
                     </div>
                     <div className="SignUp_input">
-                      <input placeholder="email" type="email" />
+                      <input placeholder="Enter Your Email" type="email" />
                     </div>
                     <div className="SignUp_input">
-                     <input placeholder="password" type="password"  /><FaEye />
+                     <input placeholder="Enter Your Password" type="password"  /><FaEye />
                     </div>
                     <div className="SignUp_input">
-                    <input type="password" placeholder="confirm password" /><FaEye />
+                    <input type="password" placeholder="Confirm Your Password" /><FaEye />
                     </div>
                 </div>
                     <div className="SignUp_submit_container">
