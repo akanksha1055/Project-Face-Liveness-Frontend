1efe72efe86c3b0a5add765760e82c193f204805
shivi028 <tiwarishivi028@gmail.com>
2024-09-15 16:34:13 +0530

    Build ForgetPassword/ResetPassword

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Login/Login';
+import ForgetPassword from './ForgetPassword/ForgetPassword';
+import ResetPassword from './ResetPassword/ResetPassword';
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/ForgetPassword" element={<ForgetPassword />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
        
       </Routes>
     </Router>
