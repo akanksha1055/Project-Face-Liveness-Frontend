baeef252bc4b161a1aef5aec9196fea944f1f23d
shivi028 <tiwarishivi028@gmail.com>
2024-09-16 16:16:31 +0530

    Navigate to pages

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import loginLogo from "../Assets/logo.png";
 import "./Login.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [visible, setVisible] = useState(false); // Start with password hidden
@@ -45,7 +46,20 @@ const Login = () => {
             </div>
             <button type="submit">Login</button>
           </form>
+
+          <div className="forget_password_div">
+          <p>
+          <Link to="/ForgetPassword" className="forget_password_link">Forget Password?</Link>
+          </p>
         </div>
+        {/* add path to signup */}
+        <div className="sign_up_div">
+          <p>
+          <Link to="/SignUp" className="sign_up_link">Want to signup?</Link>
+          </p>
+        </div>
+        </div>
+        
       </div>
     </div>
   );
