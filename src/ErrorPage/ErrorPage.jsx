649674e2d152aac4c0b717068ae5338ee1b42900
Akanksha Pal <akankshapal1055@gmail.com>
2024-09-17 13:50:07 +0530

    Error Page done

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import ErrorRobo from "../Assets/error.png";
+import Mini from "../Assets/minion.png";
 import "./ErrorPage.css";
+import { FaHouseUser } from "react-icons/fa";
 
 const ErrorPage = () => {
   return (
-    <div className='errorPage'>
-       <div className='error_txt_div'>
-            <h1>404</h1>
-            <h4>oops....</h4>
-            <h4>page not found</h4>
-       </div>
-       <div className='error_img_div'>
-            <img src={ErrorRobo} alt="errorrobo" />
-       </div>
+    <div className="Error_page">
+      <div className="minion-image">
+        <img src={Mini} alt="Minions on a Scooter" />
+      </div>
+      <div className='text_div'>
+        <h1>404</h1>
+        <p>Page not found</p>
+          
+          <button className="buttons" onClick={() => window.location.href = '/'}>
+             <span className='home_icon'><FaHouseUser /></span>
+             Home
+          </button>
+        
+      </div>
     </div>
   )
 }
